f0ccd1b6aac51eac18f23339abb5c5abed512bea
Bishwas Bhandari <bishwasbh@gmail.com>
2024-06-01 09:26:57 +0545

    log removed

--- svelte_frontend/src/routes/+page.server.ts
+++ svelte_frontend/src/routes/+page.server.ts
@@ -2,7 +2,6 @@ import {flash_redirect} from '@friendofsvelte/django-kit';
 
 
 export const load = async ({locals, cookies}) => {
-    console.log(locals.current_user)
     if (!locals.current_user) {
         flash_redirect(cookies, {
             alias: 'error',
