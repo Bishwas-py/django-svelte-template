1fa286633f499fe199f8e79f75c036fa2717d76f
Bishwas Bhandari <bishwasbh@gmail.com>
2024-06-04 16:55:17 +0545

    onclick and ondblclick added

--- svelte_frontend/src/items/ViewDate.svelte
+++ svelte_frontend/src/items/ViewDate.svelte
@@ -10,20 +10,24 @@
     let show_from_now = $state(false);
     let clk_trig = $state(false);
     // eslint-disable-next-line no-undef
-    let timeout: NodeJS.Timeout | undefined = $state();
+    let timeout: NodeJS.Timeout | number | undefined = $state();
+
+    const ondblclick = () => {
+        clearTimeout(timeout);
+        show_from_now = !show_from_now;
+        timeout = setTimeout(() => show_from_now = false, 2000);
+    };
+
+    const onclick = () => {
+        clk_trig = !clk_trig;
+        setTimeout(() => clk_trig = false, 75);
+    }
 </script>
 
 {#key show_from_now}
     <button type="button" class="{class_} {clk_trig ? 'scale-95' : ''} duration-100"
             title="Double click to toggle between from now and formatted date."
-            in:fade
-            onclick={()=>{{clk_trig = !clk_trig; setTimeout(() => clk_trig = false, 75);}}
-            }
-            ondblclick={() => {
-                clearTimeout(timeout);
-                show_from_now = !show_from_now;
-                timeout = setTimeout(() => show_from_now = false, 2000);
-            } }>
+            {onclick} {ondblclick} in:fade>
         {#if show_from_now}
             {dayjs_(date_at).fromNow()}
         {:else}
