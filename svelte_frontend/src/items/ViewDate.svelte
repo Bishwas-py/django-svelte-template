03d9563f61af070feda92da06247fff359aab47b
Bishwas Bhandari <bishwasbh@gmail.com>
2024-06-04 16:49:27 +0545

    types added

--- svelte_frontend/src/items/ViewDate.svelte
+++ svelte_frontend/src/items/ViewDate.svelte
@@ -2,20 +2,23 @@
     import {dayjs_} from "$lib";
     import {fade} from "svelte/transition";
 
-
     const {date_at, text = 'completed', class: class_ = ''}: {
         date_at: string,
         text: string,
         class?: string
     } = $props();
     let show_from_now = $state(false);
-    let timeout = $state();
+    let clk_trig = $state(false);
+    // eslint-disable-next-line no-undef
+    let timeout: NodeJS.Timeout | undefined = $state();
 </script>
 
 {#key show_from_now}
-    <button type="button" class={class_}
+    <button type="button" class="{class_} {clk_trig ? 'scale-95' : ''} duration-100"
             title="Double click to toggle between from now and formatted date."
             in:fade
+            onclick={()=>{{clk_trig = !clk_trig; setTimeout(() => clk_trig = false, 75);}}
+            }
             ondblclick={() => {
                 clearTimeout(timeout);
                 show_from_now = !show_from_now;
