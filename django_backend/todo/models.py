59b49cbb0b6cec2ee77c7896640d5488b8e5c51d
Bishwas Bhandari <bishwasbh@gmail.com>
2024-06-13 10:32:30 +0545

    started at added

--- django_backend/todo/models.py
+++ django_backend/todo/models.py
@@ -8,6 +8,7 @@ class TodoItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     completed_at = models.DateTimeField(null=True, blank=True)
+    started_at = models.DateTimeField(null=True, blank=True)
     will_complete_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
