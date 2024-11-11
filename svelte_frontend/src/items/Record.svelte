da2118315c2339de58442df3f390dd865dbc7b65
Bishwas Bhandari <bishwasbh@gmail.com>
2024-11-11 14:46:51 +0545

    chore: record not needed

--- svelte_frontend/src/items/Record.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  const {...restParams}: Record<string, string> = $props();
-</script>
-
-{#each Object.entries(restParams) as [key, value]}
- <input name="{key}" type="hidden" value="{value}"/>
-{/each}
