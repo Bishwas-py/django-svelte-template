c113a60c300464e1f4617e33895b879102ba99ac
Bishwas Bhandari <bishwasbh@gmail.com>
2024-11-12 10:27:16 +0545

    chore: djapy updated

--- django_backend/django_backend/urls.py
+++ django_backend/django_backend/urls.py
@@ -4,6 +4,12 @@ from djapy.openapi import openapi
 
 from django_backend.kit import proxy
 
+openapi.set_basic_info(
+   site_name="Django Backend",
+   title="Django Backend API",
+   description="Django Backend API Documentation",
+)
+
 urlpatterns = [
    path('', openapi.urls),
    path('admin/', admin.site.urls),
--- django_backend/home/apps.py
+++ django_backend/home/apps.py
@@ -2,14 +2,13 @@ import sys
 
 from django.apps import AppConfig
 
-from home.startup import site_data_db_startup
-
 
 class HomeConfig(AppConfig):
    default_auto_field = 'django.db.models.BigAutoField'
    name = 'home'
 
    def ready(self):
+      from home.startup import site_data_db_startup
       if ('makemigrations' not in sys.argv and 'migrate' not in sys.argv
         and 'collectstatic' not in sys.argv
         and 'createsuperuser' not in sys.argv
